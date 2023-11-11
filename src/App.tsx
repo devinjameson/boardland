130b2b8170d0ab4aef0d5ed4248d17cca30d11e2
Devin Jameson <devinjameson@hey.com>
2023-11-11 16:19:05 -0500

    Add players to board

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
 import { For } from "solid-js";
 import * as Model from "./model";
+import { pipe, Match } from "effect";
+import cn from "classnames";
 
-const board = Model.buildBoard(10, 10);
+const board = pipe(
+  Model.buildBoard(10, 10),
+  Model.updateBoard([2, 3], Model.Player1()),
+  Model.updateBoard([3, 5], Model.Player2())
+);
+
+const cellToClass = pipe(
+  Match.type<Model.Cell>(),
+  Match.tag("Empty", () => ""),
+  Match.tag("Player1", () => "bg-red-500"),
+  Match.tag("Player2", () => "bg-blue-500"),
+  Match.exhaustive
+);
 
 function App() {
   return (
     <div class="flex justify-center items-center h-screen">
       <div class="flex flex-col">
         <For each={board}>
-          {(row, rowIdx) => (
+          {(row) => (
             <div class="flex">
               <For each={row}>
-                {(cell, colIdx) => <div class="w-8 h-8 border border-black" />}
+                {(cell) => {
+                  const color = cellToClass(cell);
+                  const className = cn("w-12 h-12 border border-black", color);
+
+                  return <div class={className} />;
+                }}
               </For>
             </div>
           )}
--- src/model.ts
+++ src/model.ts
@@ -1,12 +1,29 @@
-import { Data, ReadonlyArray } from "effect";
+import { Data, ReadonlyArray, pipe } from "effect";
 
-type Board = Row[];
-type Row = Cell[];
-type Cell = Data.TaggedEnum<{ Empty: {}; Filled: {} }>;
-const { Empty, Filled } = Data.taggedEnum<Cell>();
+export type Board = Row[];
+export type Row = Cell[];
+
+export type Cell = Data.TaggedEnum<{ Empty: {}; Player1: {}; Player2: {} }>;
+
+export const { Empty, Player1, Player2 } = Data.taggedEnum<Cell>();
 
 export const buildBoard = (width: number, height: number): Board => {
   const row = ReadonlyArray.makeBy(width, () => Empty());
   const board = ReadonlyArray.makeBy(height, () => row);
   return board;
 };
+
+type ColIdx = number;
+type RowIdx = number;
+type Position = [ColIdx, RowIdx];
+
+export const updateBoard =
+  ([colIdx, rowIdx]: Position, cell: Cell) =>
+  (board: Board): Board => {
+    return pipe(
+      board,
+      ReadonlyArray.modify(rowIdx, ReadonlyArray.replace(colIdx, cell))
+    );
+  };
+
+[Empty(), Player1(), Player2()];
